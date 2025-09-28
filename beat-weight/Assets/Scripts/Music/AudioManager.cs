418c46764e3ffd25b93bbae316c10b3c8eec707d
nlia656 <nlia656@aucklanduni.ac.nz>
2025-09-28 22:48:50 +1300

    added songs and start screen to play

--- beat-weight/Assets/Scripts/Music/AudioManager.cs
+++ beat-weight/Assets/Scripts/Music/AudioManager.cs
@@ -4,17 +4,51 @@ public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
 
-    public AudioClip mainSong;
+    public AudioClip goldenClip;
+    public AudioClip livinOnAPrayerClip;
+    public AudioClip rockThatBodyClip;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void selectGolden()
     {
-        musicSource.clip = mainSong;
+        musicSource.clip = goldenClip;
         musicSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void selectLivinOnAPrayer()
     {
+        Debug.Log("selectLivinOnAPrayer called");
+        if (musicSource == null)
+        {
+            Debug.LogError("musicSource is null!");
+            return;
+        }
 
+        if (livinOnAPrayerClip == null)
+        {
+            Debug.LogError("livinOnAPrayerClip is null!");
+            return;
+        }
+
+        if (!musicSource.enabled)
+        {
+            Debug.LogWarning("musicSource is disabled. Enabling now.");
+            musicSource.enabled = true;
+        }
+
+        if (!musicSource.gameObject.activeInHierarchy)
+        {
+            Debug.LogWarning("musicSource GameObject is inactive. Activating now.");
+            musicSource.gameObject.SetActive(true);
+        }
+
+        musicSource.clip = livinOnAPrayerClip;
+        Debug.Log("Playing livinOnAPrayerClip");
+        musicSource.Play();
+    }
+
+    public void selectRockThatBody()
+    {
+        musicSource.clip = rockThatBodyClip;
+        musicSource.Play();
     }
 }
