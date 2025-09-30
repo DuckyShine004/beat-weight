0a95edf33655db4f4c3ffd46e844f02dc3aa15d7
nlia656 <nlia656@aucklanduni.ac.nz>
2025-09-30 20:44:12 +1300

    added 4th song option

--- beat-weight/Assets/Scripts/Music/AudioManager.cs
+++ beat-weight/Assets/Scripts/Music/AudioManager.cs
@@ -7,6 +7,7 @@ public class AudioManager : MonoBehaviour
     public AudioClip goldenClip;
     public AudioClip livinOnAPrayerClip;
     public AudioClip rockThatBodyClip;
+    public AudioClip believerClip;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void selectGolden()
     {
@@ -16,39 +17,19 @@ public class AudioManager : MonoBehaviour
 
     public void selectLivinOnAPrayer()
     {
-        Debug.Log("selectLivinOnAPrayer called");
-        if (musicSource == null)
-        {
-            Debug.LogError("musicSource is null!");
-            return;
-        }
-
-        if (livinOnAPrayerClip == null)
-        {
-            Debug.LogError("livinOnAPrayerClip is null!");
-            return;
-        }
-
-        if (!musicSource.enabled)
-        {
-            Debug.LogWarning("musicSource is disabled. Enabling now.");
-            musicSource.enabled = true;
-        }
-
-        if (!musicSource.gameObject.activeInHierarchy)
-        {
-            Debug.LogWarning("musicSource GameObject is inactive. Activating now.");
-            musicSource.gameObject.SetActive(true);
-        }
-
         musicSource.clip = livinOnAPrayerClip;
-        Debug.Log("Playing livinOnAPrayerClip");
         musicSource.Play();
     }
 
     public void selectRockThatBody()
     {
         musicSource.clip = rockThatBodyClip;
+        musicSource.Play();
+    }
+
+    public void selectBeliever()
+    {
+        musicSource.clip = believerClip;
         musicSource.Play();
     }
 }
