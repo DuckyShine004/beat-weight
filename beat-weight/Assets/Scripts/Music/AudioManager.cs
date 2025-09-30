a89a851e72528266aa1f8973c8ff4f1961a2e4ff
nlia656 <nlia656@aucklanduni.ac.nz>
2025-09-30 21:46:23 +1300

    offset for scoring manually done

--- beat-weight/Assets/Scripts/Music/AudioManager.cs
+++ beat-weight/Assets/Scripts/Music/AudioManager.cs
@@ -8,28 +8,38 @@ public class AudioManager : MonoBehaviour
     public AudioClip livinOnAPrayerClip;
     public AudioClip rockThatBodyClip;
     public AudioClip believerClip;
+
+    public BeatHandSyncController gameScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void selectGolden()
     {
         musicSource.clip = goldenClip;
+        gameScript.bpm = 123f;
+        gameScript.offset = -2.5f;
         musicSource.Play();
     }
 
     public void selectLivinOnAPrayer()
     {
         musicSource.clip = livinOnAPrayerClip;
+        gameScript.bpm = 123f;
+        gameScript.offset = -2.8f;
         musicSource.Play();
     }
 
     public void selectRockThatBody()
     {
         musicSource.clip = rockThatBodyClip;
+        gameScript.bpm = 125f;
+        gameScript.offset = -4.0f;
         musicSource.Play();
     }
 
     public void selectBeliever()
     {
         musicSource.clip = believerClip;
+        gameScript.bpm = 125f;
+        gameScript.offset = -4.4f;
         musicSource.Play();
     }
 }
