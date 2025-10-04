c00343f1827e10dc68c843aa72603a36e339fcef
nlia656 <nlia656@aucklanduni.ac.nz>
2025-10-04 18:27:18 +1300

    no music option, mereged main

--- beat-weight/Assets/Scripts/Music/AudioManager.cs
+++ beat-weight/Assets/Scripts/Music/AudioManager.cs
@@ -4,17 +4,51 @@ public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
 
-    public AudioClip mainSong;
+    public AudioClip goldenClip;
+    public AudioClip livinOnAPrayerClip;
+    public AudioClip rockThatBodyClip;
+    public AudioClip believerClip;
+
+    public BeatHandSyncController gameScript;
+    public BeatBlockSpawner spawner;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void SelectGolden()
     {
-        musicSource.clip = mainSong;
+        musicSource.clip = goldenClip;
+        gameScript.bpm = 123f;
+        gameScript.offset = -2.5f;
+        spawner.bpm = 123.5f;
+        spawner.delay = 0.5f;
         musicSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SelectLivinOnAPrayer()
     {
+        musicSource.clip = livinOnAPrayerClip;
+        gameScript.bpm = 122.5f;
+        gameScript.offset = -2.8f;
+        spawner.bpm = 123f;
+        spawner.delay = 0.2f;
+        musicSource.Play();
+    }
 
+    public void SelectRockThatBody()
+    {
+        musicSource.clip = rockThatBodyClip;
+        gameScript.bpm = 124.5f;
+        gameScript.offset = -4.0f;
+        spawner.bpm = 125f;
+        spawner.delay = -1.0f;
+        musicSource.Play();
+    }
+
+    public void SelectBeliever()
+    {
+        musicSource.clip = believerClip;
+        gameScript.bpm = 124.5f;
+        gameScript.offset = -4.4f;
+        spawner.bpm = 125f;
+        spawner.delay = -1.4f;
+        musicSource.Play();
     }
 }
