3710a9a1f57c647ef07105c284fb98eca5cd70d1
Gallon Zhou <gallonzhou04@gmail.com>
2025-10-07 17:36:54 +1300

    Feat: Implemented Tutorial System

---
+++ beat-weight/Assets/Scripts/Overlay/Tutorial/Tutorial.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [Header("Tutorial attributes")]
+    public GameObject[] tutorials;
+
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
