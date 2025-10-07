feddd0c803d31dcc0bd6c2051ec20da144c4cf14
Gallon Zhou <gallonzhou04@gmail.com>
2025-10-08 07:41:41 +1300

    Feat: Added Tutorial Loop

--- beat-weight/Assets/Scripts/Overlay/Tutorial/Tutorial.cs
+++ beat-weight/Assets/Scripts/Overlay/Tutorial/Tutorial.cs
@@ -5,8 +5,75 @@ public class Tutorial : MonoBehaviour
     [Header("Tutorial attributes")]
     public GameObject[] tutorials;
 
-    void Start() { }
+    [Header("References")]
+    [SerializeField]
+    private GameObject startMenu;
 
-    // Update is called once per frame
-    void Update() { }
+    private int tutorialIndex;
+
+    void Start()
+    {
+        tutorialIndex = 0;
+
+        UpdateTutorial();
+    }
+
+    void OnEnable()
+    {
+        ResetTutorialState();
+
+        UpdateTutorial();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ++tutorialIndex;
+
+            UpdateTutorial();
+        }
+    }
+
+    void UpdateTutorial()
+    {
+        if (tutorials.Length == 0)
+        {
+            return;
+        }
+
+        if (tutorialIndex > 0)
+        {
+            tutorials[tutorialIndex - 1].SetActive(false);
+        }
+
+        if (tutorialIndex < tutorials.Length)
+        {
+            tutorials[tutorialIndex].SetActive(true);
+        }
+        else
+        {
+            ResetTutorialState();
+
+            OnTutorialClose();
+        }
+    }
+
+    void ResetTutorialState()
+    {
+        foreach (var tutorial in tutorials)
+        {
+            tutorial.SetActive(false);
+        }
+
+        tutorialIndex = 0;
+    }
+
+    // CALL LAST
+    void OnTutorialClose()
+    {
+        startMenu.SetActive(true);
+
+        gameObject.SetActive(false);
+    }
 }
